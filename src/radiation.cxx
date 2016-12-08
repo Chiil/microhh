719281957590ee5d17e5bcbaa76af246de475769
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2016-12-08 17:45:53 +0100

    Completed mechanism. Threshold doesn't do much

--- src/radiation.cxx
+++ src/radiation.cxx
@@ -57,9 +57,20 @@ namespace
                                    const double* const rhoref, const double* const dz,
                                    const int istart, const int jstart, const int kstart,
                                    const int iend, const int jend, const int kend,
-                                   const int jj, const int kk)
+                                   const int jj, const int kk,
+                                   const int ijtot,
+                                   Master* master)
     {
+        // 1. Set the precipitable water field to zero.
+        for (int j=jstart; j<jend; ++j)
+             #pragma ivdep
+             for (int i=istart; i<iend; ++i)
+             {
+                  const int ij = i + j*jj;
+                  pw[ij] = 0;
+             }
 
+        // 2. Calculate the precipitable water in kg/m2 per column.
         for (int k=kstart; k<kend; ++k)
             for (int j=jstart; j<jend; ++j)
                 #pragma ivdep
@@ -70,10 +81,25 @@ namespace
                     pw[ij] += rhoref[k] * dz[k] * qt[ijk];
                 }
 
-        return 0.;
+        // 3. Calculate the mean precipitable water.
+        double pw_max = 0.;
+
+        for (int j=jstart; j<jend; ++j)
+             #pragma ivdep
+             for (int i=istart; i<iend; ++i)
+             {
+                  const int ij = i + j*jj;
+                  pw_max = std::max(pw[ij], pw_max);
+             }
+
+        master->max(&pw_max, 1);
+
+        return pw_max;
     }
 
-    void add_flex_radiation(double* const tvart, const double* const radflexprof,
+    void add_flex_radiation(double* const tvart, const double* const pw,
+                            const double* const radflexprof,
+                            const double pw_thres,
                             const int istart, const int jstart, const int kstart,
                             const int iend, const int jend, const int kend,
                             const int jj, const int kk)
@@ -83,8 +109,13 @@ namespace
                 #pragma ivdep
                 for (int i=istart; i<iend; ++i)
                 {
+                    const int ij  = i + j*jj;
                     const int ijk = i + j*jj + k*kk;
-                    tvart[ijk] += radflexprof[k];
+                    if (pw[ij] < pw_thres)
+                    {
+                        std::printf("CvH: YES, %d, %d, %E, %E\n", i, j, pw[ij], pw_thres);
+                        tvart[ijk] += radflexprof[k];
+                    }
                 }
     }
 }
@@ -145,15 +176,21 @@ void Radiation::exec()
                            grid->icells, grid->ijcells);
 
         // Calculate the precipitable water.
-        double precipitable_water_mean = calc_precipitable_water(fields->atmp["tmp1"]->databot,
-                                                                 fields->sp["qt"]->data,
-                                                                 fields->rhoref, grid->dz,
-                                                                 grid->istart, grid->jstart, grid->kstart,
-                                                                 grid->iend, grid->jend, grid->kend,
-                                                                 grid->icells, grid->ijcells);
+        double precipitable_water_max = calc_precipitable_water(fields->atmp["tmp1"]->databot,
+                                                                fields->sp["qt"]->data,
+                                                                fields->rhoref, grid->dz,
+                                                                grid->istart, grid->jstart, grid->kstart,
+                                                                grid->iend, grid->jend, grid->kend,
+                                                                grid->icells, grid->ijcells,
+                                                                grid->itot*grid->jtot,
+                                                                master);
 
+        // Here, we define dry regions where the precipitable water is less than 0.8 of the max
+        const double pw_threshold = 0.8 * precipitable_water_max;
         add_flex_radiation(fields->st["thl"]->data, 
+                           fields->atmp["tmp1"]->databot,
                            radflexprof.data(),
+                           pw_threshold,
                            grid->istart, grid->jstart, grid->kstart,
                            grid->iend, grid->jend, grid->kend,
                            grid->icells, grid->ijcells);
