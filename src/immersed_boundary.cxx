804f33d9bf4ccb3339f208e366d29b17f4e80c23
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2017-02-24 11:02:29 +0100

    Rename variables

--- src/immersed_boundary.cxx
+++ src/immersed_boundary.cxx
@@ -195,14 +195,14 @@ void Immersed_boundary::create()
             // Check whether there is an edge in range.
             const bool iface_start_in_range = (iface_start >= imin_abs) && (iface_start <  imax_abs);
             const bool iface_end_in_range   = (iface_end   >= imin_abs) && (iface_end   <  imax_abs);
-            const bool iface_full_in_range  = (iface_start <  imin_abs) && (iface_end   >= imax_abs);
+            const bool iface_fully_in_range = (iface_start <  imin_abs) && (iface_end   >= imax_abs);
 
             const bool jface_start_in_range = (jface_start >= jmin_abs) && (jface_start <  jmax_abs);
             const bool jface_end_in_range   = (jface_end   >= jmin_abs) && (jface_end   <  jmax_abs);
-            const bool jface_full_in_range  = (jface_start <  jmin_abs) && (jface_end   >= jmax_abs);
+            const bool jface_fully_in_range = (jface_start <  jmin_abs) && (jface_end   >= jmax_abs);
 
             // EAST-WEST FACES
-            if ( (jface_start_in_range || jface_end_in_range || jface_full_in_range) )
+            if ( (jface_start_in_range || jface_end_in_range || jface_fully_in_range) )
             {
                 if ( !(iface_start < imin_abs || iface_start >= imax_abs) )
                 {
@@ -236,7 +236,7 @@ void Immersed_boundary::create()
             }
 
             // NORTH-SOUTH FACES
-            if ( (iface_start_in_range || iface_end_in_range || iface_full_in_range) )
+            if ( (iface_start_in_range || iface_end_in_range || iface_fully_in_range) )
             {
                 if ( !(jface_start < jmin_abs || jface_start >= jmax_abs) )
                 {
@@ -270,8 +270,8 @@ void Immersed_boundary::create()
             }
 
             // TOP-DOWN FACES
-            if ( (jface_start_in_range || jface_end_in_range || jface_full_in_range) &&
-                 (iface_start_in_range || iface_end_in_range || iface_full_in_range) )
+            if ( (jface_start_in_range || jface_end_in_range || jface_fully_in_range) &&
+                 (iface_start_in_range || iface_end_in_range || iface_fully_in_range) )
             {
                 // Store the part of the face that is in range and add ghost cells.
                 Top_bottom_face face;
